3ecd780a9944df95c69e9c57d3bcfd896f9e1a5a
Shadowctrl <shadowctrl@outlook.com>
2024-07-23 14:12:46 +0530

    Hover

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,17 +1,40 @@
 "use client";
 import "./navbar.scss";
 import Link from "next/link";
+import { useEffect } from "react";
+
 const Navbar = ({}) => {
+  useEffect(() => {
+    const navLinks = document.querySelectorAll("#nav-item");
+    navLinks.forEach((link) => {
+      if (link.pathname === window.location.pathname)
+        link.classList.add("navbar-h2-active");
+    });
+  }, []);
   return (
     <div className="navbar-main">
       <div className="navbar-components">
-        <Link href="/">home</Link>
-        <Link href="/">on-air</Link>
-        <Link href="/">shows</Link>
-        <Link href="/">team</Link>
-        <Link href="/">links</Link>
-        <Link href="/">faqs</Link>
-        <Link href="/">contact us</Link>
+        <Link id="nav-item" href="/">
+          home
+        </Link>
+        <Link id="nav-item" href="/onair">
+          on-air
+        </Link>
+        <Link id="nav-item" href="/">
+          shows
+        </Link>
+        <Link id="nav-item" href="/">
+          team
+        </Link>
+        <Link id="nav-item" href="/">
+          links
+        </Link>
+        <Link id="nav-item" href="/">
+          faqs
+        </Link>
+        <Link id="nav-item" href="/">
+          contact us
+        </Link>
       </div>
     </div>
   );
